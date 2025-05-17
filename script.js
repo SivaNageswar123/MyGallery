a096f45a373f513133ff08128a89a1bbdbe84d8d
Siva Nageswararao Jeedimalla <sivagovardhan369@gmail.com>
2025-05-17 11:40:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-var images = document.images;
-for (var i = 1; i <= images.length; i++) {
-    images[i].src = `https://raw.githubusercontent.com/SivaNageswar123/MyGallery/refs/heads/main/${i}.jpg`;
-    images[i].style.width = '200px';
-    images[i].style.height = '200px';
+var image=document.images;
+for(var i=1;i<image.length;i++){
+    image[i].src=`https://raw.githubusercontent.com/SivaNageswar123/MyGallery/refs/heads/main/${i}.jpg`;
+    document.getElementsByTagName('img')[i].style.width='200px';
+    document.getElementsByTagName('img')[i].style.height='200px';
 }
 function bigWindow(imgpath){
-    document.getElementById('bigWindow').style.display = 'flex';
-    document.getElementById('clkImg').style.backgroundImage = `url(${imgpath})`;
-    document.getElementById("clkImg").style.backgroundRepeat = "no-repeat";
-    document.getElementById("clkImg").style.backgroundSize = "cover"; 
+    document.getElementById('bigWindow').style.display='flex';
+    document.getElementById('clkImg').style.backgroundImage=`url(${imgpath})`;
+    document.getElementById("clkImg").style.backgroundRepeat="no-repeat";
+    document.getElementById("clkImg").style.backgroundSize="cover"; 
 }
 function windowClose(){
-    document.getElementById("bigWindow").style.display = "none";
+    document.getElementById("bigWindow").style.display="none";
 }
